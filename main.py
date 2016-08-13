4379fa5e641daabcee9fb740b3113d02e937350a
julian <juojimenezca@unal.edu.co>
2016-08-13 15:11:43 -0500

    Implementación de numpy.random.choice como función de probabilidad.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from numpy.random import randint
+from numpy.random import randint, choice
 
-unidades=[1]*100 #Number of Units
+unidades=[1]*100 #Force of Units
 gamma=0.01 #Define the probability
+booleans=[True, False]
 for x in xrange(1000): #Time
     top=max(unidades)
     bottom=min(unidades)
-    probability=[]
+    suma=sum(unidades)
+    probability=[1.*y/suma for y in unidades]
+    selection=choice(unidades,1,p= probability)[0]
+    boolean=choice(booleans, 1, p=[gamma, 1-gamma])[0]
+    if boolean:        
+        unidades.remove(selection)
+        
