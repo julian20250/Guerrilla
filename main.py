5f922c78612f1974ef7b3b98f8c60d07708401e5
julian <juojimenezca@unal.edu.co>
2016-08-13 19:48:30 -0500

    Salida de datos dinámica.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from numpy.random import randint, choice
 import matplotlib.pyplot as plt
+import sys
 
 def contar(lista, number):
     count=0
@@ -8,15 +9,18 @@ def contar(lista, number):
             count+=1
     return count
 
-unidades=[1]*1000 #Force of Units
+unidades=[1]*10000 #Force of Units
 gamma=0.01 #Define the probability
 booleans=[True, False]
-for x in xrange(10000): #Time
+for x in xrange(10001): #Time
+    sys.stdout.write("\r{0}".format(str(x)))
+    sys.stdout.flush()
     top=max(unidades)
-    for y in xrange(1, top+1):
-        repeticiones=contar(unidades,y)
-        if repeticiones!=0:
-            plt.loglog(repeticiones, y, "ro")
+    if x%5==0:
+        for y in xrange(1, top+1):
+            repeticiones=contar(unidades,y)
+            if repeticiones!=0:
+                plt.loglog(repeticiones, y, "ro")
     suma=sum(unidades)
     probability=[1.*y/suma for y in unidades]
     selection=choice(unidades,1,p= probability)[0]
@@ -30,7 +34,5 @@ for x in xrange(10000): #Time
         selection2=choice(unidades, 1, p=probability2)[0]
         unidades.remove(selection2)
         unidades+=[selection+selection2]
-    else:
-        "WTF?"
         
 plt.show()
