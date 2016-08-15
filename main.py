e62fb6bb7af6a3eaf23ebdec457ae85937fcd92b
julian <juojimenezca@unal.edu.co>
2016-08-14 22:07:21 -0500

    Guardado de datos implementado.
    Una vez terminado el paso de tiempo, las unidades se
    guardan en un archivo de texto plano para ser usadas
    después.

--- main.py
+++ main.py
@@ -14,28 +14,54 @@ def contar(lista, number):
 
 
 total=10000 #Total Units
-unidades=[1]*total #Creating (total) units with force 1
+condition= input("Read datos.txt? (y/n) > ")
+
+if condition=="y":
+    f=open("datos.txt", "r")
+    unidades=f.readlines()
+    unidades=[int(x) for x in unidades]
+
+else:
+    unidades=[1]*total #Creating (total) units with force 1
+    
 gamma=0.01 #Define the probability
 booleans=[True, False]
 time = input("Digit time > ") #Time
+if time=="inf":
+    time2=3
+else:
+    time2=time
 print("When you wish to stop, use ctrl-c; all changes are saved.")
-for x in range(int(time)): 
-    sys.stdout.write("\r{0}".format("Passed %i of %s time units"%(x+1,time)))
-    sys.stdout.flush()
-    suma=sum(unidades)
-    probability=[1.*y/suma for y in unidades]
-    selection=choice(unidades,1,p= probability)[0]
-    boolean=choice(booleans, 1, p=[gamma, 1-gamma])[0]
-    if len(unidades)>1:
-        unidades.remove(selection)
-    if (boolean):
-        unidades+=[1]*selection
-    elif ((not boolean) and selection!=total):
-        probability2=[1.*y/(suma-selection) for y in unidades]
-        selection2=choice(unidades, 1, p=probability2)[0]
-        unidades.remove(selection2)
-        unidades+=[selection+selection2]
-
+x=0
+counter=0
+while (x<int(time2)): 
+    try:        
+        sys.stdout.write("\r{0}".format("Passed %i of %s time units"%(counter+1,time)))
+        sys.stdout.flush()
+        suma=sum(unidades)
+        probability=[1.*y/suma for y in unidades]
+        selection=choice(unidades,1,p= probability)[0]
+        boolean=choice(booleans, 1, p=[gamma, 1-gamma])[0]
+        if len(unidades)>1:
+            unidades.remove(selection)
+        if (boolean):
+            unidades+=[1]*selection
+        elif ((not boolean) and selection!=total):
+            probability2=[1.*y/(suma-selection) for y in unidades]
+            selection2=choice(unidades, 1, p=probability2)[0]
+            unidades.remove(selection2)
+            unidades+=[selection+selection2]
+        if time=="inf":
+            x-=1
+        x+=1
+        counter+=1
+    except KeyboardInterrupt:
+        break
+   
+f= open("datos.txt", "w")
+for x in unidades:
+    f.write(str(x)+"\n")
+f.close()
 for x in range(1, max(unidades)+1):
     repeticiones=contar(unidades,x)
     if repeticiones!=0:
